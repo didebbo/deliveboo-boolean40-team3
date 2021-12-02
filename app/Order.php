dec92e537c0b5c0ddf05167984c53a212223685a
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-12-02 23:55:17 +0700

    aggiunto order model e relazione user_order

---
+++ app/Order.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+}
