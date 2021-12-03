47ce110974b8736aeb4c612c6a0e57eb4ee83b8a
didebbo <gianluca.napoletano.93@gmail.com>
2021-12-03 11:15:39 +0100

    aggiunta migration create_dishes_table

---
+++ database/migrations/2021_12_03_095402_create_dishes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('name', 50);
+            $table->text('ingredients');
+            $table->text('description');
+            $table->float('price', 5, 2)->unsigned();
+            $table->tinyInteger('visible')->unsigned();
+            $table->string('url_picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
