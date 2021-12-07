d6af69215e1c6ba8efbef3e5f5f8252963775a95
didebbo <gianluca.napoletano.93@gmail.com>
2021-12-07 18:07:13 +0100

    fine per oggi

--- resources/js/guest/pages/Home.vue
+++ resources/js/guest/pages/Home.vue
@@ -1,14 +1,21 @@
 <template>
-	<Posts/>
+  <div>
+    <!-- Link vue-routes -->
+    <router-link to="/chi-siamo">
+      <p>Chi Siamo</p>
+    </router-link>
+    <!-- Link back-office -->
+    <a href="/merchant">Merchant</a>
+  </div>
 </template>
 
 <script>
-import Posts from '../components/Posts';
+import Posts from "../components/Posts";
 
-	export default {
-		name: 'Home',
-		components: {
-			Posts
-		}
-	}
+export default {
+  name: "Home",
+  components: {
+    Posts,
+  },
+};
 </script>
