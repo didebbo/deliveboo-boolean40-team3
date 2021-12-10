c398c0f63757c8485678e14fa124d10beefc1c44
ArisGoi <aris.goi97@gmail.com>
2021-12-10 12:22:41 +0100

    aggiunta searchPage top

--- resources/js/guest/pages/SearchPage.vue
+++ resources/js/guest/pages/SearchPage.vue
@@ -1,6 +1,47 @@
 <template>
 	<section id="Search">
-		Pagina di ricerca
+		<div id="top-search">
+			<div class="img-top-box">
+				<img src="../../../media/images/search-bg.jpg" alt="">
+				<h1>Ricerca Avanzata</h1>
+			</div>
+			<div class="search-box">
+				<!-- top-search inputs -->
+				<div class="inputs">
+					<!-- input name of restaurant -->
+					<label for="restaurant-name">Ristorante:</label>
+					<input name="restaurant-name" id="restaurant-name" type="text" placeholder="nome del ristorante">
+
+					<!-- select city -->
+					<label for="city-select">città:</label>
+					<select name="city-select" id="city-select">
+						<option value="mi">Milano</option>
+					</select>
+				</div>
+
+				<!-- bottom-search / Categories -->
+				<div class="categories">
+					<h4>Categorie:</h4>
+					<ul>
+						<li class="active">
+							<div><img src="../../../media/images/search-bg.jpg" alt=""></div>
+							Cat-1
+						</li>
+
+						<li class="active">
+							<div><img src="../../../media/images/search-bg.jpg" alt=""></div>
+							Cat-1
+						</li>
+
+						<li>
+							<div><img src="../../../media/images/search-bg.jpg" alt=""></div>
+							Cat-1
+						</li>
+					</ul>
+				</div>
+			</div>
+			<img class="search-box-after-decoration" src="../../../media/images/search-box-after-decoration.png" alt="">
+		</div>
 	</section>
 </template>
 
@@ -23,5 +64,92 @@ export default {
 
 <style scoped lang="scss">
 @import '../../../sass/_variables.scss';
+
+#top-search{
+	text-align: center;
+	.img-top-box{
+		width: 100%;
+		height: 450px;
+		overflow: hidden;
+
+		img{
+			height: 100%;
+			width: 100%;
+			object-fit: cover;
+		}
+		h1{
+			@include f-hero-title;
+		}
+	}
+
+	.search-box{
+		height: 350px;
+		width: 100%;
+		background-color: $c-03;
+		border-top-left-radius: 30px;
+		border-top-right-radius: 30px;	
+	}
+	.search-box, .search-box-after-decoration{
+		transform: translateY(-40px);
+	}
+	.search-box-after-decoration{
+		width: 100%;
+		height: 60px;
+	}
+
+	.inputs{
+		padding: 30px;
+		input, select{
+			height: 30px;
+			border: none;
+			border-radius: 20px;
+			padding: 0 10px;
+			margin-right: 25px;
+			margin-left: 5px;
+		}
+		input{
+			width: 200px;
+		}
+	}
+
+	.categories{
+		ul{
+			@include flex-center;
+			flex-wrap: wrap;
+			list-style: none;
+
+			li{
+				cursor: pointer;
+				position: relative;
+				margin: 15px;
+				padding: 10px 20px 10px 55px;
+				border-radius: 10px;
+				background-color: $c-05;
+				color: $text-light-white;
+
+				div{
+					position: absolute;
+					height: 50px;
+					width: 50px;
+					border-radius: 50%;
+					left: -5px;
+					top: 50%;
+					overflow: hidden;
+					transform: translateY(-50%);
+					@include img-fit-cover;
+					background-color: $c-04;
+					border: 3px solid white;
+				}
+
+				&.active{
+					filter: drop-shadow($shadow-05);
+					box-shadow: 0px 1px 0px $c-05;
+					background-color: $c-02;
+					color: black;
+				}
+			}
+		}
+	}
+}
 
 </style>
