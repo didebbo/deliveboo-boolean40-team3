da6e84c1ff6b5384a50323700649e94004dff41b
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-12-10 11:49:30 +0700

    aggiunta api dettagli ristorante

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\User;
 use Illuminate\Database\Eloquent\Builder;
+use App\User;
+use App\Dish;
 
 class RestaurantController extends Controller
 {
@@ -44,15 +45,36 @@ class RestaurantController extends Controller
         else {
             $users = $users->get();
         }
-        dd($users);
+        
+        // controllo se la query ha trovato almeno un user(ristorante) e restituisce file json
+        if ($users->isNotEmpty())  {
+            return response()->json([
+                'success' => true,
+                'data' => $users
+            ]);
+        }
         return response()->json([
-            'success' => true,
-            'data' => $users
+            'success' => false,
+            'data' => null
         ]);
     }
 
-    public function show(Request $request)
+    public function show($id)
     {
-        dd($request->all());
+        // query per restituire il ristorante desiderato
+        $user = User::where('id', $id)->first();
+        
+        // controllo se la query ha trovato almeno un user(ristorante) e restituisce file json
+        if ($user){
+            $dishes = Dish::where('user_id', $id)->get();
+            return response()->json([
+                'success' => true,
+                'data' => ['user'=>$user, 'dishes'=> $dishes]
+            ]);
+        }
+        return response()->json([
+            'success' => false,
+            'data' => null
+        ]);
     }
 }
