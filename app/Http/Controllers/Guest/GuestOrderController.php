426d4ecf485afb60cd7c68fd452f6bff91db10cd
didebbo <gianluca.napoletano.93@gmail.com>
2021-12-15 15:51:50 +0100

    refactoring GuestOrderController

--- app/Http/Controllers/Guest/GuestOrderController.php
+++ app/Http/Controllers/Guest/GuestOrderController.php
@@ -18,39 +18,6 @@ class GuestOrderController extends Controller
         'order.notes' => 'nullable|max:255'
     ];
 
-    protected $validator2 = [
-        'customer_email' => 'required|email|max:255',
-        'customer_firstname' => 'required|string|max:50',
-        'customer_lastname' => 'required|string|max:50',
-        'customer_phone' => 'required|string|max:20',
-        'customer_address' => 'required|string|max:150',
-        'notes' => 'nullable|max:255'
-    ];
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('guest.orders.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate($this->validator2);
-        $newOrder = Order::create($request->all());
-        // $newOrder->dishes()->attach($request['categories']);
-        return redirect()->route('guest.index');
-    }
-
     public function checkout(Request $request)
     {
         $request->validate($this->validator);
