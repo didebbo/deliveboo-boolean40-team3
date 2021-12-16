872467cd81f8939c4ad6f192dd51acf99aae3a19
ArisGoi <aris.goi97@gmail.com>
2021-12-16 11:19:58 +0100

    styled 404 notFound

--- resources/js/guest/pages/NotFound.vue
+++ resources/js/guest/pages/NotFound.vue
@@ -1,6 +1,9 @@
 <template>
 	<div id="notfound-page">
-		<h1>Errore 404: Pagina non trovata</h1>
+		<div>
+			<img src="../../../media/images/notfound-tomato.png" alt="">
+			<h1>Errore 404: Pagina non trovata</h1>
+		</div>
 	</div>
 </template>
 
@@ -12,11 +15,23 @@
 
 <style scoped lang="scss">
 @import '../../../sass/_variables.scss';
-h1{
-	// margin: 400px 0;
-}
 
 #notfound-page{
-	
+	padding-top: $top-heigth;
+	@include flex-center;
+	min-height: calc(100vh - 211px);
+	>div{
+		@include flex-center;
+		img{
+			max-width: 650px;
+			filter: drop-shadow(0px 0px 8px 0px rgba(0,0,0,0.7));
+		}
+		h1{
+			color: white;
+			position: relative;
+			left: -70px;
+			font-size: 3.5rem;
+		}
+	}
 }
 </style>
