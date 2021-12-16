928ac2cffbfc7e8f64c5ac46f19598fdc4d06f02
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-12-16 22:09:12 +0700

    aggiunta favicon e title

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -14,9 +14,17 @@ $clientToken = $gateway->clientToken()->generate();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="clientToken" content="{{ $clientToken }}">
-    <title>Document</title>
+    <title>Deliveboo</title>
+
+    {{-- favicon --}}
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <meta name="msapplication-TileColor" content="#31BFA0">
+    <meta name="theme-color" content="#31BFA0">
+
 </head>
 
 <body>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -19,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/merchant.css') }}" rel="stylesheet">
+    
 </head>
 
 <body>
