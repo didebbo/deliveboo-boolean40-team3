f02ed541d271dd675cfeb1df81289b16163c449c
ArisGoi <aris.goi97@gmail.com>
2021-12-20 10:50:55 +0100

    changed categories img

--- resources/js/guest/components/PopCategories.vue
+++ resources/js/guest/components/PopCategories.vue
@@ -22,7 +22,7 @@
                     {{category.name}}
                 </p>
                 <div class="food">
-                    <img src="../../../media/images/fast-food-2.jpg" alt="Panino Americano">
+                    <img src="../../../media/images/search-bg.jpg" alt="Panino Americano">
                 </div>
                 <div class="search_icon">
                     <img src="../../../media/icons/search-white.svg" alt="Icona Ricerca">
